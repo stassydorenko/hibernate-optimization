a63fe32c0b2df442b0fc376c8ed259f77e7b77e6
Stanislav_Sydorenko <Stanislav.Sydorenko@nordstrom.com>
2021-12-28 10:29:43 +0200

    initial commit

---
+++ src/main/java/com/sydorenko/test/hibernate/HibernateOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.sydorenko.test.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateOptimizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sydorenko/test/hibernate/HibernateOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sydorenko.test.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
